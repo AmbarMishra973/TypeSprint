da8551d516f54463427c672df5dbfacb9e30008e
Ambar Mishra <mishraambar19@gmail.com>
2026-08-03 20:14:05 +0530

    "Initialize React frontend structure for TypeSprint"

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>⌨ TypeSprint</h1>
+            <p>Improve your typing speed every day.</p>
+        </div>
+    );
+}
+
+export default Home;
