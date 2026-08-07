ac07d5ba5b0b7c2e8ca4accbdf22b93567ce71b9
Ambar Mishra <mishraambar19@gmail.com>
2026-08-07 09:53:34 +0530

    changing api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:8080/api";
+const API_BASE_URL = "https://ambarmishradb.onrender.com/api";
 
 // Helper function for POST requests
 // Replace your postData function in src/services/api.js with this:
