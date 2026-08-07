1ff53e355bccdef0e83c94bf8df61f2a2a37bb4d
Ambar Mishra <mishraambar19@gmail.com>
2026-08-07 13:34:34 +0530

    fixing errors

--- backend/src/main/java/com/ambar/portfolio/model/User.java
+++ backend/src/main/java/com/ambar/portfolio/model/User.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
+import jakarta.persistence.Lob;
 
 @Entity
 @Table(name = "users")
